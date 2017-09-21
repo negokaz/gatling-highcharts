175bfaa0a1f0e6e3b6dea979e5dc1135920e4659
Stephane Landelle <slandelle@gatling.io>
2017-09-21 14:31:15 +0200

    Upgrade scalariform 1.8.0 and formatting rules

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,12 +4,14 @@ object Dependencies {
 
   private val bundleArtifact = Artifact("gatling-bundle", "zip", "zip", "bundle")
 
+  // format: OFF
   def charts(gatlingVersion: String)      = "io.gatling"     % "gatling-charts"   % gatlingVersion
   def app(gatlingVersion: String)         = "io.gatling"     % "gatling-app"      % gatlingVersion
   def recorder(gatlingVersion: String)    = "io.gatling"     % "gatling-recorder" % gatlingVersion
   def scalaCompiler(scalaVersion: String) = "org.scala-lang" % "scala-compiler"   % scalaVersion   % "runtime"
   def compiler(gatlingVersion: String)    = "io.gatling"     % "gatling-compiler" % gatlingVersion % "zinc"
   def bundle(gatlingVersion: String)      = "io.gatling"     % "gatling-bundle"   % gatlingVersion % "runtime" artifacts bundleArtifact
+  // format: ON
 
   def gatlingChartsHighchartsDeps(version: String) =
     Seq(charts _, app _, recorder _).map(_(version))
